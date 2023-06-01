e83fc3269ad48a67e4476bf7ff327b4a4e950cad
KATO Kei <k4t0.kei@gmail.com>
2023-06-01 23:42:55 +0900

    feat: Commit a commit.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # rollup-plugin-eco
 Rollup plugin for eco template
+
+# Usage
+
+```js
+import eco from 'rollup-plugin-eco';
+
+export default {
+  input: 'src/main.js',
+  output: {
+    file: 'bundle.js',
+    format: 'cjs'
+  },
+  plugins: [eco()]
+}
+```
---
+++ index.js
@@ -0,0 +1,12 @@
+const eco = require('eco')
+
+const ecoPlugin = () => {
+  return {
+    transform: (code, id) => {
+      if (!id.endsWith('.eco')) return null;
+      return { code: `export default ${eco.precompile(code)}` };
+    },
+  };
+};
+
+module.exports = ecoPlugin;
